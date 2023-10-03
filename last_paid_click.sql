672980eddb783fca73f052c0fe1d44154255a0b6
GHMan2021 <87323431+GHMan2021@users.noreply.github.com>
2023-10-03 22:30:54 +0400

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,36 +1,3 @@
-/*
-select 
-	l.visitor_id,
-	s.visit_date,
-	s."source",
-	s.medium,
-	s.campaign,
-	l.lead_id,
-	l.created_at,
-	l.amount ,
-	l.closing_reason ,
-	l.status_id 
-from sessions as s
-left join leads l  
-	on s.visitor_id = l.visitor_id
---where l.amount >= 0
-order by l.amount desc, s.visit_date */
-
-/*
-select 
-	s.visitor_id,
-	s.visit_date,
-	
-	l.lead_id,
-	l.created_at
-	--age(s.visitor_id, l.created_at) as delta
-from sessions s
-left join leads l 
-on s.visitor_id = l.visitor_id
-where l.lead_id is not null  
-order by s.visitor_id , s.visit_date
-limit 150;*/
-
 select
     s.visitor_id,
     s.visit_date,
@@ -39,13 +6,17 @@ select
     s.campaign,
     l.lead_id,
     l.created_at,
-    l.amount ,
-    l.closing_reason ,
-    l.status_id 
+    l.amount,
+    l.closing_reason,
+    l.status_id
 from sessions as s
 full join leads as l
     on s.visitor_id = l.visitor_id
-order by l.amount desc nulls last , s.visit_date , s."source" , s.medium , s.campaign  
+where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+order by
+    l.amount desc nulls last,
+    s.visit_date asc,
+    s.source asc,
+    s.medium asc,
+    s.campaign asc
 limit 10
- 
-
