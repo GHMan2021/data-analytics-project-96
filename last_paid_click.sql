186a2b579b279544fc320a83ce8e59758f581b1d
GHMan2021 <87323431+GHMan2021@users.noreply.github.com>
2023-10-07 01:04:46 +0400

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,22 +1,45 @@
+with tab as (
+    select
+        s.visitor_id,
+        s.visit_date,
+        s.source,
+        s.medium,
+        s.campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        last_value(s.visit_date)
+            over (
+                partition by l.lead_id
+                order by
+                    s.visit_date
+                range between current row and unbounded following
+            )
+        as lv
+    from sessions as s
+    full join leads as l
+        on s.visitor_id = l.visitor_id
+    where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+)
 select
-    s.visitor_id,
-    s.visit_date,
-    s.source,
-    s.medium,
-    s.campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-from sessions as s
-full join leads as l
-    on s.visitor_id = l.visitor_id
-where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    visitor_id,
+    visit_date,
+    source,
+    medium,
+    campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from tab
+where visit_date = lv
 order by
-    l.amount desc nulls last,
-    s.visit_date asc,
-    s.source asc,
-    s.medium asc,
-    s.campaign asc
+    amount desc nulls last,
+    visit_date asc,
+    source asc,
+    medium asc,
+    campaign asc
 limit 10
